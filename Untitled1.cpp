68431ee4292426c784e7c98de0896c2f5c409354
dawidprzychodzki <dawid.przychodzki08@gmail.com>
2023-10-24 14:59:55 +0200

    Komentarz

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main (){
+ 	
+ 	short zmienna = 2;
+	cout << "a"
+	cout << endl;
+	cout << "wszystko wina przemka"
+	cout << endl;
+	cout << "liczba" << zmienna;
+	
+	return 0;
+}
