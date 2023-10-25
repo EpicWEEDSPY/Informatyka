b2792adb08bbb2e8e25b1fa9646351c355cc551f
dawidprzychodzki <dawid.przychodzki08@gmail.com>
2023-10-25 15:04:01 +0200

    Komentarz

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,13 +3,33 @@
 using namespace std;
  
 int main (){
- 	
+ 	// deklaracja zminnych
  	short zmienna = 2;
-	cout << "a"
+ 	float zmianaprzecinkowa = 3.21;
+ 	string tekst = "tekst";
+ 	char znak = 'A';
+ 	bool logiczny = false;
+ 	
+	cout << "a";
 	cout << endl;
-	cout << "wszystko wina przemka"
-	cout << endl;
-	cout << "liczba" << zmienna;
+	cout << "wszystko wina przemka";
+	cout << endl; // koniec lini
+	cout << "liczba" << zmienna; //wyswiewtlanie zmiennych
 	
+	string wyraz;
+	cout << endl;
+	cout << "Podaj jakis wyraz ";
+	cin >> wyraz;
+	cout << "Podales wyraz " << wyraz;
+	
+	//imie
+	string imie;
+	float wiek;
+	cout << "podaj imie ";
+	cin >> imie;	
+	cout << endl;
+	cout << "poidaj wiekj ";
+	cin >> wiek;
+	cout << "masz na imie " << imie << " i masz " << wiek << " lat";
 	return 0;
 }
