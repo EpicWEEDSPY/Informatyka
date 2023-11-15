b1fce7cb4d2c36fe714952d56698b0c1d4a70308
dawidprzychodzki <dawid.przychodzki08@gmail.com>
2023-11-15 15:03:50 +0100

    komentarz

---
+++ Prostokąt.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+	using namespace std;
+
+int main(){
+	short szerokosc;
+	cout << "jaka ma byc szerokosc ";
+	cin >> szerokosc;
+short wysokosc;
+	cout << "jaka ma byc wysokosc ";
+	cin >> wysokosc;
+   
+   for(int i=1; i < 5 i++){
+       for(int j=1; j < 11; j++){
+         
+			cout << "*";
+       }
+       
+       cout << endl;
+   }
+}
